b83e340648502adc5e689987f028e410e43f3b28
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2022-10-24 17:24:44 -0400

    Eliminate categories index on payments table, and eliminate payments foreign key on categories

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,8 @@
 class Payment < ApplicationRecord
   belongs_to :user
-  belongs_to :category
+  has_and_belongs_to_many :categories
+
+  validates :name, presence: true, length: { minimum: 1, maximum: 40 }
+  validates :amount, presence: true
+  validates :amount, numericality: { greater_than_or_equal_to: 0 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_22_205844) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_24_211240) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,7 +35,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_22_205844) do
     t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_payments_on_category_id"
     t.index ["user_id"], name: "index_payments_on_user_id"
   end
 
@@ -57,6 +56,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_22_205844) do
   end
 
   add_foreign_key "categories", "users"
-  add_foreign_key "payments", "categories"
   add_foreign_key "payments", "users"
 end
