4a48552d19b446e10d6a2e8734e0cbd1afc83ba0
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2022-10-25 08:38:46 -0400

    Remove user_id from new category form, and add current_user to Category index method, and to Category create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
-  #before_action :authenticate_user!
-  before_action :set_user, only: %i[ show edit update destroy ]
+  # before_action :authenticate_user!
+  before_action :set_user, only: %i[show edit update destroy]
 
   def welcome
+    redirect_to categories_path if user_signed_in?
   end
-
 
   # GET /users or /users.json
   def index
@@ -12,8 +12,7 @@ class UsersController < ApplicationController
   end
 
   # GET /users/1 or /users/1.json
-  def show
-  end
+  def show; end
 
   # GET /users/new
   def new
@@ -21,8 +20,7 @@ class UsersController < ApplicationController
   end
 
   # GET /users/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /users or /users.json
   def create
@@ -30,7 +28,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to categories_url(@user), notice: "User was successfully created." }
+        format.html { redirect_to user_url(@user), notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -43,7 +41,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to categories_url(@user), notice: "User was successfully updated." }
+        format.html { redirect_to user_url(@user), notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -57,19 +55,20 @@ class UsersController < ApplicationController
     @user.destroy
 
     respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:name)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
--- spec/views/payments/show.html.erb_spec.rb
+++ spec/views/payments/show.html.erb_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "payments/show", type: :view do
+RSpec.describe 'payments/show', type: :view do
   before(:each) do
     @payment = assign(:payment, Payment.create!(
-      name: "Name",
-      amount: 2.5,
-      user: nil,
-      category: nil
-    ))
+                                  name: 'Name',
+                                  amount: 2.5,
+                                  user: nil,
+                                  category: nil
+                                ))
   end
 
-  it "renders attributes in <p>" do
+  it 'renders attributes in <p>' do
     render
     expect(rendered).to match(/Name/)
     expect(rendered).to match(/2.5/)
